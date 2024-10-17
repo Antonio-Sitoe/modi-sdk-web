58fe5384fc98bb1eae7c874779b5fd9132b260a3
Antonio Sitoe <antoniositoehl@gmail.com>
2024-10-17 22:32:21 +0200

    as

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,11 +1,11 @@
-import axios from 'axios';
-import cancelTokenManager from '../utils/cancelrequest';
-import { netErrorConections, retryRequest } from '@/utils/net-conexions';
+import axios from 'axios'
+import cancelTokenManager from '../utils/cancelrequest'
+import { netErrorConections, retryRequest } from '@/utils/net-conexions'
 
-const token = '$2b$10$LbK5CowvIw/41hisDp0douLnlIkD/LpbTnlkCq2TEoqEGUCQalmGa';
-const authorization = 'Bearer ' + token;
+const token = '$2b$10$LbK5CowvIw/41hisDp0douLnlIkD/LpbTnlkCq2TEoqEGUCQalmGa'
+const authorization = 'Bearer ' + token
 
-const api_base = 'http://localhost:3000';
+const api_base = import.meta.env.VITE_API_BASE
 
 const api = axios.create({
   baseURL: api_base,
@@ -13,26 +13,26 @@ const api = axios.create({
     Authorization: authorization,
     'Content-Type': 'multipart/form-data',
   },
-});
+})
 
-api.interceptors.request.use(config => {
-  config.cancelToken = cancelTokenManager.createToken();
-  return config;
-});
+api.interceptors.request.use((config) => {
+  config.cancelToken = cancelTokenManager.createToken()
+  return config
+})
 
-api.interceptors.response.use(undefined, error => {
+api.interceptors.response.use(undefined, (error) => {
   if (error instanceof axios.AxiosError) {
-    const errorMessage = netErrorConections[error.code as string] || null;
+    const errorMessage = netErrorConections[error.code as string] || null
     if (errorMessage) {
-      const retryButton = document.querySelector('.offline-ui-retry');
-      retryButton?.addEventListener('click', async e => {
-        e.preventDefault();
-        await retryRequest(error);
-      });
+      const retryButton = document.querySelector('.offline-ui-retry')
+      retryButton?.addEventListener('click', async (e) => {
+        e.preventDefault()
+        await retryRequest(error)
+      })
     }
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-  return Promise.reject(error);
-});
+  return Promise.reject(error)
+})
 
-export { cancelTokenManager, api };
+export { cancelTokenManager, api }
