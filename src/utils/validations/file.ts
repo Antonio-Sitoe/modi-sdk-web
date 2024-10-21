9a9f5b6ef26053fb097109869b9517b8b36b2b82
Antonio Sitoe <antoniositoehl@gmail.com>
2024-10-21 18:24:35 +0200

    Update file.ts

--- src/utils/validations/file.ts
+++ src/utils/validations/file.ts
@@ -3,10 +3,12 @@ import { z } from 'zod'
 const ACCEPTED_FILE_TYPES = ['.docx', '.pdf', '.doc', '.jpg', '.jpeg', '.png']
 export const fileSchema = z
   .any()
-  .refine((files) => files?.length >= 1, { message: 'Arquivo é obrigatório.' })
+  .refine((files) => !!(files?.length >= 1), {
+    message: 'Arquivo é obrigatório.',
+  })
   .refine(
     (files) => {
-      return ACCEPTED_FILE_TYPES.some((typeDocs) => files.includes(typeDocs))
+      return ACCEPTED_FILE_TYPES.some((typeDocs) => files?.includes(typeDocs))
     },
     {
       message: 'Apenas arquivos .pdf, .doc e .docx são aceitos.',
